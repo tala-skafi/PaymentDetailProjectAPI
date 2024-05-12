0e0892835a26ed95052fb322d683f38b60bad1bd
Tala <tala1998@gmail.com>
2024-05-12 23:40:58 +0300

    Initial commit for API

---
+++ Models/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
